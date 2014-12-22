ce7d4b96da98927d88b1122b20e2d4fc8c8677e8
Rick Jones <rick.jones@saatchionline.com>
2014-12-21 17:00:00 -0800

    playing around with Laravel

--- app/views/rwd.php
+++ app/views/rwd.php
@@ -33,8 +33,6 @@
 	<div class="large-12 columns">
 		<div class="panel">
 			<h3><em>Some Basic Styles from the F5 installation</em></h3>
-			<p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
-			<p>Once you've exhausted the fun in this document, you should check out:</p>
 			<div class="row">
 				<div class="large-4 medium-4 columns">
 					<p><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</p>
